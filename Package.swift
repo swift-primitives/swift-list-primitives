692e488d9a9f74e6a6510bc1d15d2805ca7c7c9e
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-21 10:23:33 +0100

    Add Collection Primitives dependency
    Lists are collections and should depend on Collection Primitives for
    collection view types and transformations.

--- Package.swift
+++ Package.swift
@@ -17,9 +17,19 @@ let package = Package(
             targets: ["List Primitives"]
         )
     ],
+    dependencies: [
+        .package(path: "../swift-index-primitives"),
+        .package(path: "../swift-input-primitives"),
+        .package(path: "../swift-collection-primitives"),
+    ],
     targets: [
         .target(
-            name: "List Primitives"
+            name: "List Primitives",
+            dependencies: [
+                .product(name: "Index Primitives", package: "swift-index-primitives"),
+                .product(name: "Input Primitives", package: "swift-input-primitives"),
+                .product(name: "Collection Primitives", package: "swift-collection-primitives"),
+            ]
         ),
         .testTarget(
             name: "List Primitives Tests",
