4dbf7dad87fb39690507682aca4d12019191cb39
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:07:22 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-list-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         // Root: namespace + foundational, stdlib-only declarations (zero deps).
@@ -33,7 +33,10 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-index-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-index-primitives.git",
+            branch: "main"
+        )
     ],
     targets: [
         // MARK: - Root
@@ -80,7 +83,7 @@ let package = Package(
         .testTarget(
             name: "List Primitives Tests",
             dependencies: [
-                "List Primitives",
+                "List Primitives"
             ]
         ),
     ],
@@ -102,7 +105,7 @@ for target in package.targets where ![.system, .binary, .plugin, .macro].contain
     ]
 
     let package: [SwiftSetting] = [
-        .enableExperimentalFeature("RawLayout"),
+        .enableExperimentalFeature("RawLayout")
     ]
 
     target.swiftSettings = (target.swiftSettings ?? []) + ecosystem + package
